68c71f92df07d1b4648f851f175728e86c1a83a1
Marcos Chiari <mchiari25@Mac.localdomain>
2025-07-29 12:08:43 -0500

    Agrego ruta /clientes de prueba

--- autogo_erp/main.py
+++ autogo_erp/main.py
@@ -6,3 +6,10 @@ app = FastAPI()
 def read_root():
     return {"message": "AutoGo ERP Demo funcionando desde main.py!"}
 
+@app.get("/clientes")
+def get_clientes():
+    return [
+        {"id": 1, "nombre": "Cliente A", "email": "clienteA@autogo.com"},
+        {"id": 2, "nombre": "Cliente B", "email": "clienteB@autogo.com"}
+    ]
+
