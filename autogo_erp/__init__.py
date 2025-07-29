f26a673abbeedd2d9c82398a4eb12d221283779a
Marcos Chiari <mchiari25@Mac.localdomain>
2025-07-29 11:46:09 -0500

    Corrijo error de indentación

--- autogo_erp/__init__.py
+++ autogo_erp/__init__.py
@@ -5,10 +5,6 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "AutoGo ERP Demo funcionando!"}
- UW PICO 5.09                           File: autogo_erp/__init__.py                           Modified  
-
-# Paquete del ERP AutoGo basado en FastAPI
-    
     
     
     
